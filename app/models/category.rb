8eefaeab535b1a26cf499a2999d681ea3c01620b
Diego Gomez <diegogc@LAPTOP-8ABLQL06>
2023-04-20 18:43:59 -0500

    add: validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
     has_many :photos
     has_many :comments, as: :commentable, dependent: :destroy
+
+    validates :name, present: true, unique: true, length:{maximun: 25}
+    validates :description, length:{in: 25..200}, allow_blank: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_232818) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_233737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_232818) do
     t.integer "photo_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
