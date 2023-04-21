7498a40b67024f170ddb7e00bda9d2856ff45bb6
Diego Gomez <diegogc@LAPTOP-8ABLQL06>
2023-04-21 15:51:46 -0500

    add: Seed categories, comments, photos

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
     has_many :photos
     has_many :comments, as: :commentable, dependent: :destroy
 
-    validates :name, present: true, unique: true, length:{maximun: 25}
+    validates :name, presence: true, uniqueness: true, length:{ in: 1..25 }
     validates :description, length:{in: 25..200}, allow_blank: true
 end
