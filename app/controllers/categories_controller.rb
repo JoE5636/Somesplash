d2e06239d0b2cfd16499a9e1b103408f6d784326
Gustavout22 <gustavougartetorres@gmail.com>
2023-04-21 18:42:39 -0500

    Add: Category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,51 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @photos = @category.photos
+  end
+
+  def create
+    @category = Category.new(category_params)
+  
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to department_path(@category)
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+
+    redirect_to_categories_path, status: :see_other
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'photos/new'
+  get 'photos/show'
+  get 'photos/create'
+  get 'photos/edit'
+  get 'photos/update'
+  get 'photos/destroy'
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/show'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
