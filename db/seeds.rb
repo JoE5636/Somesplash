36596d5cbf592279d4ef11b79a6ab415c49aecb6
Diego Gomez <diegogc@LAPTOP-8ABLQL06>
2023-04-21 15:24:56 -0500

    add: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
+puts "Seeding - START"
+
 Comment.destroy_all
 Photo.destroy_all
 Category.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!('categories')
-ActiveRecord::Base.connection.reset_pk_sequence!('photos')
 ActiveRecord::Base.connection.reset_pk_sequence!('comments')
+ActiveRecord::Base.connection.reset_pk_sequence!('photos')
+ActiveRecord::Base.connection.reset_pk_sequence!('categories')
