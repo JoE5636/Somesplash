7d6a27503bbbbf35ca456a20e772168acfe4acfe
Rossio Lachos <rossio.lachos28@gmail.com>
2023-04-22 01:07:31 -0400

    Add: photo controller new create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,20 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
+    @category = Category.find(params[:category_id])
   end
 
   def show
+    
   end
 
   def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photo_path(@photo)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -15,5 +24,11 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :category_id, :image)
   end
 end
