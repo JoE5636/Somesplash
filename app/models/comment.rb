cdd5852fabb7e92541dceb1efb8d5a0dec9c8faa
Rossio Lachos <rossio.lachos28@gmail.com>
2023-04-23 16:57:30 -0400

    Add: counter and show the counter of photos comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,22 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   validates :body, presence: true
+
+  after_create :increment_comments_count
+  after_destroy :decrement_comments_count
+
+  private
+
+  def increment_comments_count
+    if commentable_type == "Photo"
+      commentable.increment!(:comments_count)
+
+    end
+  end
+
+  def decrement_comments_count
+    if commentable_type == "Photo"
+      commentable.decrement!(:comments_count)
+    end
+  end
 end
